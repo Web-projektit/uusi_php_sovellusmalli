a0280d57bb20b2ee1858d1f7575204d5dc7cc276
Web-projektit <jukka.aula@omnia.fi>
2024-10-15 13:40:02 +0300

    korjattu login.php

--- login.php
+++ login.php
@@ -12,7 +12,6 @@ if ($loggedIn = loggedIn()) {
     }
 $title = 'Kirjautuminen';
 $css = 'login.css';
-$js = 'login.js';
 /* Lomakkeen kentät, nimet samat kuin users-taulussa. */
 $kentat = ['email','password','rememberme'];
 $kentat_suomi = ['sähköpostiosoite','salasana','muista minut'];
